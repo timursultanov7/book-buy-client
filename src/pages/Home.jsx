d1158dee77a26e1e970058f041613575813ae91c
timursultanov7 <timursultanov7@gmail.com>
2023-01-23 22:03:59 +0200

    deleted error-boundary from home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ErrorBoundary from "../components/ErrorBoundary";
 
 import { LayoutImage } from "../components/LayoutImage";
 import { BestSellers } from "../components/BestSellers";
@@ -8,15 +7,11 @@ import { Categories } from "../components/Categories";
 export const Home = () => {
   return (
     <div>
-      <ErrorBoundary>
-        <LayoutImage />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <BestSellers />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Categories />
-      </ErrorBoundary>
+      <LayoutImage />
+
+      <BestSellers />
+
+      <Categories />
     </div>
   );
 };
